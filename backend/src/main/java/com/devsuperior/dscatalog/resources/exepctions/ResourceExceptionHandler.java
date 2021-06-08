dfdd9c70a0e31bcd2c90dcde1fcba556acfb08db
Ramon Fernandes <ramonmfb777@gmail.com>
2021-06-08 16:22:39 -0300

    Category findById, exepction handling

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exepctions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscatalog.resources.exepctions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dscatalog.services.exepctions.EntityNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setError("Resource not found!");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+}

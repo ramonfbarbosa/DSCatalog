a751e0624eb2b1779c15bfd2bbe46e2f94cddaff
Ramon Barbosa <ramonmfb777@gmail.com>
2021-08-06 12:36:04 -0300

    Product entity, SQL script

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,15 +2,20 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
@@ -94,8 +99,4 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	}
-	
-	
+}

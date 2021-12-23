0e92f67ad6e31ce67d07b776cf81eb0168ab7ac2
Ramon Barbosa <ramonmfb777@gmail.com>
2021-12-22 21:23:59 -0300

    Home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,11 @@
 import "./assets/styles/custom.scss";
 import "./App.css";
-import Navbar from "components/Navbar";
+import Home from "pages/Home";
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <h4>DSCatalog</h4>
+    <Home />
     </>
   );
 }

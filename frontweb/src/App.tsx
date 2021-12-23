07a0a88770e46cb5b631156d1ccd695c39a4b507
Ramon Barbosa <ramonmfb777@gmail.com>
2021-12-23 15:13:11 -0300

    Routes

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,13 +1,14 @@
 import "./assets/styles/custom.scss";
 import "./App.css";
-import Home from "pages/Home";
+
+import Routes from "Routes";
 
 const App = () => {
   return (
     <>
-    <Home />
+      <Routes />
     </>
   );
-}
+};
 
 export default App;
--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,32 +1,31 @@
 import { ReactComponent as MainImage } from "assets/imgs/main-image.svg";
 import ButtonIcon from "components/ButtonIcon";
-import Navbar from "components/Navbar";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 const Home = () => {
   return (
-    <>
-      <Navbar />
-      <div className="home-container">
-        <div className="home-card">
-          <div className="home-content-container">
-            <div>
-              <h1>Conheça o melhor catálogo de produtos</h1>
-              <p>
-                Ajudaremos você a encontrar os melhores produtos disponíveis no
-                mercado.
-              </p>
-            </div>
-            <div>
-              <ButtonIcon />
-            </div>
+    <div className="home-container">
+      <div className="base-card home-card">
+        <div className="home-content-container">
+          <div>
+            <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>
+              Ajudaremos você a encontrar os melhores produtos disponíveis no
+              mercado.
+            </p>
           </div>
-          <div className="home-image-container">
-            <MainImage />
+          <div>
+            <Link to="/products">
+              <ButtonIcon />
+            </Link>
           </div>
         </div>
+        <div className="home-image-container">
+          <MainImage />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
